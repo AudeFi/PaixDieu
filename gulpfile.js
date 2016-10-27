8b6c7d457974890694a99f74958f14d0e4373ee2
Aude Ficheux <Aude Ficheux>
2016-10-27 17:37:34 +0200

    add[gulpfile]: html includes & px to rem

--- gulpfile.js
+++ gulpfile.js
@@ -10,25 +10,52 @@ var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
 var autoprefixer = require('gulp-autoprefixer');
+var fileinclude = require('gulp-file-include');
+/*var modernizr = require('gulp­modernizr');*/
+var postcss = require('gulp-postcss');
+var pxtorem = require('postcss-pxtorem');
 
 
-gulp.task('watch', ['browserSync', 'sass'], function(){
+gulp.task('watch', ['browserSync', 'sass', 'fileinclude'], function(){
+  // Run sass compilation task whenever SASS files change
   gulp.watch('app/scss/**/*.scss', ['sass']);
+  // Run html file includes task whenever HTML files change
+  gulp.watch('app/includes/**/*.html', ['fileinclude']);
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('app/*.html', browserSync.reload); 
   gulp.watch('app/js/**/*.js', browserSync.reload); 
 });
 
+// Compiles all HTML files included to one index.html
+gulp.task('fileinclude', function() {
+  gulp.src(['app/includes/index.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+// Converts Sass to CSS and prefix
 gulp.task('sass', function(){
+  var processors = [
+        pxtorem({
+            replace: false,
+            propWhiteList: []
+        })
+  ];
+
   return gulp.src('app/scss/main.scss')
-    .pipe(sass()) // Using gulp-sass
+    .pipe(sass())
     .pipe(autoprefixer())
+    .pipe(postcss(processors))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
+// Hot reload 
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
@@ -37,8 +64,20 @@ gulp.task('browserSync', function() {
   })
 });
 
+// NOT CALLED YET 
+gulp.task('modernizr', function() {
+  gulp.src(['./js/**/*.js', './css/**/*.css'])
+    .pipe(modernizr({
+      "options" : [
+        "setClasses", "addTest", "html5printshiv", "testProp", "fnBind"
+        ]
+  }))
+  .pipe(gulp.dest("build/"))
+}); 
+
+// Minifies JS & CSS each into one file and them copy them and the html to /dist folder
 gulp.task('useref', function(){
-  return gulp.src('app/*.html')
+  return gulp.src('app/index.html')
     .pipe(useref())
     // Minifies only if it's a JavaScript file
     .pipe(gulpIf('*.js', uglify()))
@@ -47,6 +86,7 @@ gulp.task('useref', function(){
     .pipe(gulp.dest('dist'))
 });
 
+// min the images and cache them
 gulp.task('images', function(){
   return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
   // Caching images that ran through imagemin
@@ -56,24 +96,29 @@ gulp.task('images', function(){
   .pipe(gulp.dest('dist/images'))
 });
 
+// Copy the fonts to the /dist folder
 gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
   .pipe(gulp.dest('dist/fonts'))
 });
 
+/* Clean the /dist folder*/
 gulp.task('clean:dist', function() {
   return del.sync('dist');
 });
 
+
+/* Default task which compiles sass and html includes, run hot reload and watch for changes */
 gulp.task('default', function (callback) {
-  runSequence(['sass','browserSync', 'watch'],
+  runSequence(['fileinclude', 'sass','browserSync', 'watch'],
     callback
   )
 });
 
+/* Build task which compiles scss, html includes, js, minify css & js & images */
 gulp.task('build', function (callback) {
-  runSequence('clean:dist', 
-    ['sass', 'useref', 'images', 'fonts'],
+  runSequence('clean:dist',
+    ['fileinclude', 'sass', 'images', 'fonts'], 'useref',
     callback
   )
 });
