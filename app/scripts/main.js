f2e8417e9713005e314ca45067ab313f234e45f7
Aude Ficheux <Aude Ficheux>
2016-11-09 14:09:22 +0100

    add[navigation]: Swiper, menu with current page, detection of changing url

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,35 @@
-//Navigation scroll
-var menu_link = document.querySelectorAll('.main-menu a');
-for(var i = 0; i < menu_link.length; i++) {
-    menu_link[i].addEventListener('click',function(event){
-        var $anchor = $(this);         
-        $('html, body').stop().animate({
-            scrollLeft: $($anchor.attr('href')).offset().left
-        }, 1000);
-        event.preventDefault();
-    });
+/*
+ *
+ * HORIZONTAL NAVIGATION
+ *
+ */
+var mySwiper = new Swiper ('.swiper-container', { // Init swiper
+    direction: 'horizontal',
+    loop: false,
+    hashnav:true,
+    nextButton: '.swiper-button-next',
+});
+
+// EVENTS
+
+//When I swipe manually (the hash change and I change the active menu item)
+window.onhashchange = function(){ 
+    var hash=document.location.hash.split('#')[1];
+    $( ".swiper-pagination-switch."+ hash ).trigger( "click" );
+};
+
+//When I click on a section in the menu
+$('.swiper-pagination-switch').on("click", changeSlide); 
+
+// Change the active menu item and swipe to the correct slide 
+function changeSlide(){
+    console.log("click");
+    mySwiper.slideTo($(this).index());
+    $('.swiper-pagination-switch').removeClass('active');
+    $(this).addClass('active')
 }
+
+
+// Get the position in the page at first connection on the website and update the position
+var hash=document.location.hash.split('#')[1];
+$( ".swiper-pagination-switch."+ hash ).trigger( "click" );
