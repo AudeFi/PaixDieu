e2a41f75a93d1bbb2456a09b919ce4a796a6da88
Aude <odefi@hotmail.fr>
2016-11-17 15:22:37 +0100

    add[navigation]: event on menu appearance

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -30,6 +30,7 @@ window.onhashchange = function(){
 };
 //When I click on a section in the menu
 $('.swiper-pagination-switch').on("click", function () {
+    mySwiper.unlockSwipeToNext(); //After opening the menu, unlock the ability to swipe next
     swipeEvent( $(this) );
 });
 
@@ -50,4 +51,54 @@ $('.swiper-pagination-switch').removeClass('active');
 $('.swiper-pagination-switch.'+ hash ).addClass('active');
 
 // SWIPER CONTROLS
-/*$('.swiper-button-next')*/
+/*var pressTimer;
+$('.swiper-button-next').mouseup(function(){
+    if( $cancelclick ){
+        mySwiper.lockSwipeToNext(); // If we oppened the menu with a long press on next button, don't swipe to next slide
+    }
+    else {
+        mySwiper.unlockSwipeToNext(); // If we didn't open the menu, act normal and swipe next
+    }
+    clearTimeout(pressTimer);
+    return false;
+}).mousedown(function(){
+    $cancelclick = false;
+    pressTimer = window.setTimeout(function() { 
+        $('ul.main-menu').toggle('pressed');
+        console.log("long click");
+        $cancelclick = true;
+    },500);    
+    return false; 
+});
+*/
+
+
+//determine which events to use
+var startEventType = 'mousedown',
+    endEventType   = 'mouseup';
+//If mobile
+    startEventType = 'touchstart';
+    endEventType   = 'touchend';
+
+//bind to determined event(s)
+var pressTimer;
+$('.swiper-button-next').bind(endEventType, function() {
+    if( $cancelclick ){
+        console.log("lock");
+        mySwiper.lockSwipeToNext(); // If we oppened the menu with a long press on next button, don't swipe to next slide
+    }
+    else {
+        console.log("not lock");
+        mySwiper.unlockSwipeToNext(); // If we didn't open the menu, act normal and swipe next
+    }
+    clearTimeout(pressTimer);
+    return false;
+}).bind(startEventType, function(){
+    $cancelclick = false;
+    pressTimer = window.setTimeout(function() { 
+        $('ul.main-menu').toggle('pressed');
+        console.log("long click");
+        $cancelclick = true;
+    },500);    
+    return false; 
+});
