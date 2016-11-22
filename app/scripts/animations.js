6560c9c658a5c9325287505ba6749dc4abd0d4a7
vanessachaddouk <vanessa.chaddouk@gmail.com>
2016-11-22 22:29:39 +0100

    add[slider]scroll events

---
+++ app/scripts/animations.js
@@ -0,0 +1,31 @@
+if (document.querySelector('.swiper-container')!=undefined) {
+    /*
+     *
+     * VERTICAL SCROLL
+     *
+     */
+
+ 	console.log('I am animations.js');	
+ 	
+ 	document.querySelector('.swiper-slide-active').onclick = function(){
+ 		var element = document.querySelector('.swiper-slide-active');
+ 		console.log('clic on element');
+ 		console.log(element + element.scrollTop);	
+ 	}
+
+	document.querySelector('.swiper-slide-active').addEventListener('scroll', function() {
+	        console.log('scrolling');
+	
+	  	if (document.querySelector('.swiper-slide-active').scrollTop > 50) {
+	  		console.log('scroll supérieur à 10');
+	  		document.querySelector('.swiper-slide-active').style.padding = "0px";
+	  	}
+	  	if (document.querySelector('.swiper-slide-active').scrollTop < 49) {
+	  		console.log('scroll inférieur à 0');
+	  		document.querySelector('.swiper-slide-active').style.padding = "50px";
+	  	}
+	});
+	
+
+
+}
