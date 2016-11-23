7560e318b09da67c5f33374913b2b639a203a35c
Aude <odefi@hotmail.fr>
2016-11-23 19:33:34 +0100

    add[frame]: bottom part of frame animation

--- app/scripts/animations.js
+++ app/scripts/animations.js
@@ -4,24 +4,18 @@ if (document.querySelector('.swiper-container')!=undefined) {
      * VERTICAL SCROLL
      *
      */
-
- 	console.log('I am animations.js');	
- 	
- 	document.querySelector('.swiper-slide-active').onclick = function(){
- 		var element = document.querySelector('.swiper-slide-active');
- 	}
-
 	document.querySelector('.swiper-slide-active').addEventListener('scroll', function() {
-	        console.log('scrolling');
+	   console.log('scrolling');
 	
 	  	if (document.querySelector('.swiper-slide-active').scrollTop > 50) {
 	  		document.querySelector('.logo').style.margin = "-340px 40px";
-	  		document.querySelector('.swiper-slide-active').style.padding = "0px";
+            document.querySelector('.swiper-slide-active').style.padding = "0px";
+	  		document.querySelector('.controls_bgAnimation').style.height = "0";
 	  	}
 	  	if (document.querySelector('.swiper-slide-active').scrollTop < 49) {
 	  		document.querySelector('.swiper-slide-active').style.padding = "50px 20px";
 	  		document.querySelector('.logo').style.margin = "40px 40px";
-
+            document.querySelector('.controls_bgAnimation').style.height = "100%";
 	  	}
 	});
 	
