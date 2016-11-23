85405cb0451997bc041e1ef86af0d63fc4b2ce65
Anthelme RISSER <anthelme.risser@gmail.com>
2016-11-23 22:02:19 +0100

    html, css, js[contact + main] : Mobile next button =>  Last slide opacity less improve responsive contact page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,6 +21,7 @@ if (document.querySelector('.swiper-container')!=undefined) {
     //When I swipe manually (the hash change and I change the active menu item)
     window.onhashchange = function(){ 
         var hash = document.location.hash.split('#')[1];
+        var controls_button = document.querySelector('.controls_button');
         if (hash.indexOf("?") != -1 )
             hash = hash.substring(0, hash.indexOf("?"));
         var currentSwitch = document.querySelector('.swiper-pagination-switch.'+ hash );
@@ -34,9 +35,14 @@ if (document.querySelector('.swiper-container')!=undefined) {
         else {
             swipeEvent(currentSwitch);
         }
-        if (hash == "brassins")
+        if (hash == "brassins"){
             urlParameters(paramBrassin);
-        document.querySelector('.controls_pageTitle').innerHTML = document.querySelector('.swiper-pagination-switch.active').textContent;    
+            controls_button.style.opacity = 0.5;
+        }
+        else{
+            controls_button.style.opacity = 1;
+        }
+        document.querySelector('.controls_pageTitle').innerHTML = document.querySelector('.swiper-pagination-switch.active').textContent;
     };
     //When I click on a section in the menu
     var allSwitcher = document.querySelectorAll('.swiper-pagination-switch');
@@ -81,7 +87,7 @@ if (document.querySelector('.swiper-container')!=undefined) {
     if (hash.indexOf("brassins?") != -1 ) { // If i have parameters on a specific bottle
         paramBrassin = hash.substring(hash.indexOf("=") + 1); //Save parameter
         hash = hash.substring(0, hash.indexOf("?")); // Get the hash without parameter
-        swipeEvent( document.querySelector('.swiper-pagination-switch.'+ hash ) ); // Force swipe to the page
+        swipeEvent( document.querySelector('.swiper-pagination-switch.'+ hash ) ); // Force swipe to the page        
     } else if (hash.indexOf("deguster?") != -1 ) { // If i have parameters on a specific bar
         paramBrasserie = hash.substring(hash.indexOf("=") + 1); //Save parameter
         hash = hash.substring(0, hash.indexOf("?")); // Get the hash without parameter
@@ -126,6 +132,7 @@ if (document.querySelector('.swiper-container')!=undefined) {
             menuState = "close";
         } else {
             pressTimer = window.setTimeout(function() { 
+                console.log('longpress');
                 addClass('.controls_menuOpen', 'openned');
                 menuState = "open";
                 mySwiper.lockSwipes(); // If we oppened the menu with a long press on next button, don't swipe to next slide
