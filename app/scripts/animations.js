f22b4c15c9429aceb2bbd8bda9affe35f8013ed0
vanessachaddouk <vanessa.chaddouk@gmail.com>
2016-11-23 16:21:08 +0100

    remove[animations]console log on scroll

--- app/scripts/animations.js
+++ app/scripts/animations.js
@@ -9,20 +9,16 @@ if (document.querySelector('.swiper-container')!=undefined) {
  	
  	document.querySelector('.swiper-slide-active').onclick = function(){
  		var element = document.querySelector('.swiper-slide-active');
- 		console.log('clic on element');
- 		console.log(element + element.scrollTop);	
  	}
 
 	document.querySelector('.swiper-slide-active').addEventListener('scroll', function() {
 	        console.log('scrolling');
 	
 	  	if (document.querySelector('.swiper-slide-active').scrollTop > 50) {
-	  		console.log('scroll supérieur à 10');
 	  		document.querySelector('.logo').style.margin = "-340px 40px";
 	  		document.querySelector('.swiper-slide-active').style.padding = "0px";
 	  	}
 	  	if (document.querySelector('.swiper-slide-active').scrollTop < 49) {
-	  		console.log('scroll inférieur à 0');
 	  		document.querySelector('.swiper-slide-active').style.padding = "50px 20px";
 	  		document.querySelector('.logo').style.margin = "40px 40px";
 
